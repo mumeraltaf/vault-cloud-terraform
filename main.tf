920d3a24289d8924e92f0ca0a8d054f59fa1d53d
Umer <umer.altaf@unimelb.edu.au>
2022-10-04 14:31:35 +1100

    test instance creation and env file

--- main.tf
+++ main.tf
@@ -30,15 +30,16 @@ data "vault_kv_secret_v2" "openstack-app-creds" {
   name  = "openstack-app-creds"
 }
 
+data "vault_kv_secret_v2" "envfile" {
+  mount = "secret"
+  name  = "envfile"
+}
+
 provider "openstack" {
   auth_url                      = data.vault_kv_secret_v2.openstack-app-creds.data.OS_AUTH_URL
   application_credential_id     = data.vault_kv_secret_v2.openstack-app-creds.data.OS_APPLICATION_CREDENTIAL_ID
   application_credential_secret = data.vault_kv_secret_v2.openstack-app-creds.data.OS_APPLICATION_CREDENTIAL_SECRET
   region                        = data.vault_kv_secret_v2.openstack-app-creds.data.OS_REGION_NAME
-}
-
-data "openstack_compute_instance_v2" "instance" {
-  id = "51ddb9d1-af27-49ff-a014-312d6c2d094d"
 }
 
 resource "vault_mount" "terraform-secrets" {
@@ -60,4 +61,29 @@ resource "vault_kv_secret" "ssh_key" {
 }
 
 
+
+resource "openstack_compute_keypair_v2" "adp-terraform-network-key" {
+  name       = "adp-terraform-network"
+  public_key = tls_private_key.ssh.public_key_pem
+}
+
+resource "openstack_compute_instance_v2" "test-instance" {
+  name            = "test-instance"
+  flavor_name     = "r3.xsmall"
+  image_name      = "NeCTAR Debian 11 (Bullseye) amd64"
+  key_pair        = openstack_compute_keypair_v2.adp-terraform-network-key.name
+  security_groups = ["default", "ssh-int", "https-int", "https-all"]
+
+  connection {
+    user        = "debian"
+    host        = openstack_compute_instance_v2.test-instance.access_ip_v4
+    private_key = tls_private_key.ssh.private_key_pem
+  }
+
+  provisioner file {
+    content = data.vault_kv_secret_v2.envfile.data.env
+    destination = "/home/debian/.env"
+  }
+
+}
 
